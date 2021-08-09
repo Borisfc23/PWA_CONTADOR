ab39bc6e78c4d18868b4b54e5a698ec6785c96ad
BorisFlores <borisfc23@gmail.com>
2021-08-09 16:00:17 -0500

    mi primera pwa con react

---
+++ Components/Contador.js
@@ -0,0 +1,13 @@
+const Contador = ()=>{
+    const [contador,setContador] = React.useState(0);
+    const aumentar = ()=> setContador(contador + 1)
+    const disminuir = ()=> setContador(contador - 1)
+    return(
+        <div>
+            <h1 className={contador < 0 ? "menor":"mayor"} >CONTADOR : {contador}</h1>
+            <hr></hr>
+            <button onClick={aumentar}>Aumentar</button>
+            <button onClick={disminuir}>Disminuir</button>
+        </div>
+    )
+}
---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_ELEMENTS = [
+    "./", //cachear la pagina de inicio
+    "https://unpkg.com/react@17/umd/react.production.min.js",
+    "https://unpkg.com/react-dom@17/umd/react-dom.production.min.js",
+    "https://unpkg.com/@babel/standalone/babel.min.js",
+    "./Components/Contador.js",
+    "./style.css"
+]
+
+const CACHE_NAME = "v1_cache_contador_react"
+//Nota el self es similar al this
+
+/*=========== Instalar el Service worker  ============*/
+self.addEventListener("install",(e)=>{
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache =>{
+            cache.addAll(CACHE_ELEMENTS).then(()=>{
+                self.skipWaiting()
+            }).catch(err=>console.log(err))
+        })
+    )
+});
+
+/*=========== Activar el Service Worker  ============*/
+self.addEventListener('activate',(e)=>{
+    const cacheWhiteList = [CACHE_NAME]
+    e.waitUntil(
+        caches.keys().then(cacheNames=>{
+            return Promise.all(cacheNames.map(cacheName=>{
+                /*=========== Elimina caches repetidas  ============*/
+                return(                                 //los && transformar el operador ternario en solo valor positico osea solo si cumple
+                    cacheWhiteList.indexOf(cacheName)===-1 && caches.delete(cacheName)
+                )
+            }))
+        }).then(()=>self.clients.claim())
+    )
+});
+
+
+self.addEventListener("fetch",(e)=>{    
+    e.respondWith(
+        caches.match(e.request).then((res)=>{
+            if(res){
+                return res
+            }else{
+                return fetch(e.request);
+            }
+        })
+    )
+});
